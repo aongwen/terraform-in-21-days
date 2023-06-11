3f9545d3b5091239b471cccae79983170210e31f
aongwen <ongwen@gmail.com>
2023-06-11 13:17:59 +0300

    Add VPC, subnets, IG, NAT and route

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "main"
+    Name = "main_vpc"
   }
 }
 
@@ -50,7 +50,7 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main"
+    Name = "main_IG"
   }
 }
 
